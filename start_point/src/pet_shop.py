00fc04b5dd73f0a2111e8a61d8eac248d8685ec3
klamb95 <klamb1995@gmail.com>
2021-04-02 14:24:30 +0100

    get stock count

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -17,8 +17,9 @@ def increase_pets_sold(pet_shop, pets):
     pet_shop['admin']['pets_sold'] += pets
     return pet_shop['admin']['pets_sold']
 
-
-
+def get_stock_count(pet_shop):
+    return len(pet_shop['pets'])
+    
 
 
     
