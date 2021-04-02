b695d02c308fe43ecb70ae4f0398f32e2828def6
klamb95 <klamb1995@gmail.com>
2021-04-02 14:03:44 +0100

    first commit

--- start_point/src/pet_shop.py
+++ start_point/src/pet_shop.py
@@ -1,1 +1,37 @@
 # WRITE YOUR FUNCTIONS HERE
+   
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop['admin']['total_cash'] += cash
+    return pet_shop['admin']['total_cash']
+
+def get_pets_sold(pet_shop):
+    return pet_shop['admin']['pets_sold']
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+   
+        
+
+
+
+
+
+
+    
